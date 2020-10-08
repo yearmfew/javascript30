99b1cc6924adc7d17135a86021433011d53bbe33
yearmfew <brlylmz23@gmail.com>
2020-10-08 18:30:22 +0200

    Project 22 is done
    Project 22 is finished. It is a nice property. I am gonna make a clone of it.
    
    Project 19, 20 and 21 is not working in my computer.
    There is a problem when I try to reach my media such as mikrophone and video.
    I will not froce to make these project. I am just moving on with the next ones.

---
+++ js30-22/custom.js
@@ -0,0 +1,31 @@
+// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+// Variables
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+// functions
+
+function highlightLink(){
+	const linkCoords = this.getBoundingClientRect();
+	console.log(linkCoords);
+	const coords = {
+		width: linkCoords.width,
+		height: linkCoords.height,
+		top: linkCoords.top + window.scrollY,
+		left: linkCoords.left + window.scrollX
+	};
+
+
+	highlight.style.width = `${coords.width}px`;
+	highlight.style.height = `${coords.height}px`;
+	highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+
+
+
+}
+
+// Event Listeners
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
