15efed0edc55ff13b25023a363fa88c2ee104d67
yearmfew <brlylmz23@gmail.com>
2020-10-10 16:07:03 +0200

    Project 27 is finished.

---
+++ js30-27/custom.js
@@ -0,0 +1,37 @@
+// Variables 
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX, scrollLeft;
+
+
+// Functions
+
+
+
+// Event Listeners
+
+slider.addEventListener('mousedown', (e) => {
+	isDown = true;
+	slider.classList.add('active');
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+	isDown = false;
+	slider.classList.remove('active');
+
+});
+
+slider.addEventListener('mouseup', () => {
+	isDown = false;
+	slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+	if(!isDown) return; // stop if it is not clicked
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = (x - startX) * 3;
+	slider.scrollLeft = scrollLeft - walk;
+});
