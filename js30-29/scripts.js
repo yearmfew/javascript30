2cd820afbb2a0226f09c7f61df527b02977be48d
yearmfew <brlylmz23@gmail.com>
2020-10-11 22:05:01 +0200

    Project29 is done It is finished. I am moving to the next one. I wanna finish these tutorial :):):):)

---
+++ js30-29/scripts.js
@@ -0,0 +1,62 @@
+// Variables
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const input = document.customForm;
+
+
+// Functions
+function timer(seconds){
+	// clear any existing timers
+	clearInterval(countdown);
+
+	const now = Date.now();
+	const then = now + seconds * 1000;
+	displayTimeLeft(seconds);
+	displayEndTime(then);
+
+	countdown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		// check if we should stop it ! 
+		if(secondsLeft < 0){
+			clearInterval(countdown);
+			return;
+		}
+		 // display it
+		 displayTimeLeft(secondsLeft);
+		}, 1000);
+}
+
+function displayTimeLeft(seconds){
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display =`${minutes}: ${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+	document.title = display;
+	timerDisplay.textContent = display;
+	console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp){
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const adjustedHour = hour > 12 ? hour - 12 : hour;
+	const minutes = end.getMinutes();
+	endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0': ''}${minutes}`;
+}
+
+function startTimer(){
+	seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+
+function inputStart(e){
+	e.preventDefault();
+	const mins = this.minutes.value;
+	timer(mins * 60);
+	this.reset();
+}
+// Event Listeners
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+input.addEventListener('submit', inputStart);
